37b68260ab355246937cd475fc74a2e5abd16e8a
Daniel Böök <Daniel.book@hotmail.com>
2016-01-23 16:33:01 +0100

    Added shaders, and some sort of nice skybox

--- src/assets/js/scene.js
+++ src/assets/js/scene.js
@@ -1,60 +1,131 @@
-var container, stat, clock;
+var container, stat;
 
-var camera, scene, controls, renderer;
+var camera, scene, controls, renderer, dirLight, hemiLight;
 
-var group, text, plane;
+var group;
+
+var icoMesh;
+
+var clock = new THREE.Clock();
 
 init();
 animate();
 
 function init() {
+    container = document.getElementById( 'container' );
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
-    camera.position.z = 5;
-    camera.position.y = 3;
+    camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 1, 5000 );
+    camera.position.set( 0, 0, 250 );
 
     scene = new THREE.Scene();
-    clock = new THREE.Clock();
 
+    scene.fog = new THREE.Fog( 0xffffff, 1, 5000 );
+    scene.fog.color.setHSL( 0.6, 0, 1 );
+
+    /*
     controls = new THREE.FlyControls( camera );
-		controls.movementSpeed = 10;
-		controls.domElement = container;
-		controls.rollSpeed = Math.PI / 24;
-		controls.autoForward = false;
-		controls.dragToLook = false;
+	controls.movementSpeed = 10;
+	controls.domElement = container;
+	controls.rollSpeed = Math.PI / 24;
+	controls.autoForward = false;
+	controls.dragToLook = false;
+    */
 
-    renderer = new THREE.WebGLRenderer();
+    // LIGHTS
+    hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
+    hemiLight.color.setHSL( 0.6, 1, 0.6 );
+    hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+    hemiLight.position.set( 0, 500, 0 );
+    scene.add( hemiLight );
+
+    //
+
+    dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
+    dirLight.color.setHSL( 0.1, 1, 0.95 );
+    dirLight.position.set( -1, 1.75, 1 );
+    dirLight.position.multiplyScalar( 50 );
+    scene.add( dirLight );
+
+    dirLight.castShadow = true;
+
+    dirLight.shadowMapWidth = 2048;
+    dirLight.shadowMapHeight = 2048;
+
+    var d = 50;
+
+    dirLight.shadowCameraLeft = -d;
+    dirLight.shadowCameraRight = d;
+    dirLight.shadowCameraTop = d;
+    dirLight.shadowCameraBottom = -d;
+
+    dirLight.shadowCameraFar = 3500;
+    dirLight.shadowBias = -0.0001;
+    //dirLight.shadowCameraVisible = true;
+
+    // GROUND
+    var groundGeo = new THREE.PlaneBufferGeometry( 10000, 10000 );
+    var groundMat = new THREE.MeshPhongMaterial( { color: 0xffffff, specular: 0x050505 } );
+    groundMat.color.setHSL( 0.095, 1, 0.75 );
+
+    var ground = new THREE.Mesh( groundGeo, groundMat );
+    ground.rotation.x = -Math.PI/2;
+    ground.position.y = -33;
+    scene.add( ground );
+
+    ground.receiveShadow = true;
+
+    // SKYDOME
+    var vertexShader = document.getElementById( 'vertexShader' ).textContent;
+    var fragmentShader = document.getElementById( 'fragmentShader' ).textContent;
+    var uniforms = {
+        topColor:    { type: "c", value: new THREE.Color( 0x0077ff ) },
+        bottomColor: { type: "c", value: new THREE.Color( 0xffffff ) },
+        offset:      { type: "f", value: 33 },
+        exponent:    { type: "f", value: 0.6 }
+    };
+    uniforms.topColor.value.copy( hemiLight.color );
+
+    scene.fog.color.copy( uniforms.bottomColor.value );
+
+    var skyGeo = new THREE.SphereGeometry( 4000, 32, 15 );
+    var skyMat = new THREE.ShaderMaterial( { vertexShader: vertexShader, fragmentShader: fragmentShader, uniforms: uniforms, side: THREE.BackSide } );
+
+    var sky = new THREE.Mesh( skyGeo, skyMat );
+    scene.add( sky );
+
+
+    geometry = new THREE.IcosahedronGeometry( 1, 1 );
+    var material = new THREE.MeshPhongMaterial( { color: 0xffffff, specular: 0xffffff, shininess: 20, morphTargets: true, vertexColors: THREE.FaceColors, shading: THREE.FlatShading } );
+    icoMesh = new THREE.Mesh( geometry, material );
+
+    icoMesh.position.set(0,0,-10);
+
+    icoMesh.castShadow = true;
+    icoMesh.receiveShadow = true;
+    scene.add( icoMesh );
+
+    // RENDERER
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setClearColor( scene.fog.color );
+    renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+    container.appendChild( renderer.domElement );
 
-    var geometry = new THREE.PlaneGeometry( 30, 30);
-    var material = new THREE.MeshBasicMaterial( { color: 0xBCAF46 } );
-    var plane = new THREE.Mesh( geometry, material );
-    scene.add( plane );
+    renderer.gammaInput = true;
+    renderer.gammaOutput = true;
 
-    plane.rotation.x = -90;
-
-    geometry = new THREE.IcosahedronGeometry( 3, 1 );
-    material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
-    var ico = new THREE.Mesh( geometry, material );
-    scene.add( ico );
-
-    ico.position.y = 3;
-    ico.position.z = -10;
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.cullFace = THREE.CullFaceBack;
 
     camera.position.z = 5;
     window.addEventListener( 'resize', onWindowResize, false );
 }
 
 function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-        windowHalfX = window.innerWidth / 2;
-        windowHalfY = window.innerHeight / 2;
-
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-
-        renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
 /**
@@ -62,6 +133,9 @@ function onWindowResize() {
 */
 function animate() {
         requestAnimationFrame( animate );
+
+        icoMesh.rotation.y += 0.01;
+        icoMesh.rotation.x += 0.01;
 
         render();
 }
@@ -72,6 +146,7 @@ function animate() {
 function render() {
         var delta = clock.getDelta();
 
-        controls.update( delta );
+        //controls.update( delta );
+        
         renderer.render( scene, camera );
 }
